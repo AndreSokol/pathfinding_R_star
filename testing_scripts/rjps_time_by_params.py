cd46eb1b1f3620946678e5af2b82207d21f92e1a
Andrey Sokolov <sokolov-as-i@yandex.ru>
2017-03-20 14:37:23 +0300

    added fitting option

--- testing_scripts/rjps_time_by_params.py
+++ testing_scripts/rjps_time_by_params.py
@@ -7,8 +7,9 @@ import plotly.graph_objs as go
 
 import pandas as pd
 
-TEMPLATE_ENDING = """
-"""
+import numpy as np
+
+from copy import deepcopy
 
 DIST_TO_SUCC_VALUE_RANGE = list(range(5, 151, 5))
 NUMBER_OF_SUCC_VALUE_RANGE = list(range(10, 301, 10))
@@ -127,8 +128,103 @@ def plot():
     return
 
 
+def fit():
+    z_data = pd.read_csv('stats.csv', index_col=0)
+
+    z_data_old = deepcopy(z_data)
+
+    x = list(z_data.keys())
+    print(*x)
+    y = list(z_data[z_data.keys()[0]].keys())
+    print(*y)
+
+    for i in range(len(x)):
+        args = np.polyfit(z_data[x[i]].keys(), z_data[x[i]], 3)
+        for y_1 in y:
+            z_data[x[i]][y_1] = args[0] * (y_1 ** 3) + args[1] * (y_1 ** 2) + args[2] * y_1 + args[3]
+
+    z_data = z_data.transpose()
+
+    x = list(z_data.keys())
+    print(*x)
+    y = list(z_data[z_data.keys()[0]].keys())
+    print(*y)
+
+    for i in range(len(x)):
+        args = np.polyfit(list(map(float, z_data[x[i]].keys())), list(z_data[x[i]]), 3)
+        for y_1 in y:
+            z_data[x[i]][y_1] = args[0] * (int(y_1) ** 3) + args[1] * (int(y_1) ** 2) + args[2] * int(y_1) + args[3]
+
+    z_data = z_data.transpose()
+
+    data = [
+        go.Surface(
+            z=z_data.as_matrix(),#z_data2
+            x=z_data.columns,
+            y=z_data.index,
+            colorscale=[[0, 'rgb(230,0,0)'], [1, 'rgb(20,20,20)']],
+            zmax=1,
+            zmin=0,
+            opacity=0.9999,
+        ),
+
+        go.Surface(
+            z=z_data_old.as_matrix(),#z_data2
+            x=z_data_old.columns,
+            y=z_data_old.index,
+            colorscale=[[0, 'rgb(230,230,230)'], [1, 'rgb(20,20,20)']],
+            zmax=1,
+            zmin=0,
+            opacity=1
+        ),
+    ]
+
+    layout = go.Layout(
+    title="Test",#'R* heatmap, metrics=' + test_stats[1] + ', hweight=' + test_stats[2] + ', average of ' + test_stats[3],
+    xaxis= dict(
+    title= 'Distance to successors',
+    #ticklen= 5,
+    #dtick=1,
+    #ticks='outside',
+    ),
+    yaxis=dict(
+    title= 'Number of successors',
+    #ticklen= 5,
+    #dtick=1,
+    #ticks='outside',
+    ),
+    scene=dict(
+    xaxis= dict(
+    title= 'Distance to successors',
+    ),
+    yaxis=dict(
+    title= 'Number of successors',
+    ),
+    zaxis=dict(
+    title= 'Time',
+    ),
+    ),
+    autosize=True,
+    #width=600,
+    #height=600,
+
+    #margin=dict(
+    #    l=65,
+    #    r=50,
+    #    b=65,
+    #    t=90
+    #    )
+    )
+    fig = go.Figure(data=data, layout=layout)
+    plotly.offline.plot(fig, filename='elevations-3d-surface')
+
+    return
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 1:
         main()
     elif sys.argv[1] == '-plot':
         plot()
+    elif sys.argv[1] == '-fit':
+        fit()
